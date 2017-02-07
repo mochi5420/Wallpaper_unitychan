0e2aba8a2bc3cb549cde720a1c079e11c307d305
Keisuke Mochida <k.mochida5420@gmail.com>
2017-02-08 04:24:55 +0900

    Play the Video!!
    動画を流せるようにしました

---
+++ Assets/Script/ManageMovieTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageMovieTexture : MonoBehaviour {
+
+
+    public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+
+        movie.Play();
+        movie.loop = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
