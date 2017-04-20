3dbd2671feaf370c9a4080cbb1e2bc6d14290149
Keisuke Mochida <k.mochida5420@gmail.com>
2017-04-20 14:48:23 +0900

    sound関連のデバッグをオフにした

---
+++ Assets/Script/MicrophoneInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[DisallowMultipleComponent]		//複数アタッチさせない.
+
+public class MicrophoneInput : MonoBehaviour {
+
+    private new AudioSource audio;
+
+    float loudness;
+
+    public float GetLoudness()
+    {
+        return loudness;
+    }
+
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 10, 44100); 
+        audio.loop = true;
+        //audio.mute = true;
+        while (!(Microphone.GetPosition("") > 0)){ } // Wait until the recording has started
+        audio.Play();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        loudness = GetAveragedVolume() *10;
+        //Debug.Log(loudness);
+	}
+
+
+    float GetAveragedVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        audio.GetOutputData(data, 0);
+        foreach (float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+        return a / 256;
+    }
+}
---
+++ Assets/Script/aaa.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aaa : MonoBehaviour {
+
+    MicrophoneInput mpi;
+
+    // Use this for initialization
+    void Start()
+    {
+        mpi = GameObject.FindObjectOfType<MicrophoneInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float l = mpi.GetLoudness();
+        Debug.Log(l);
+        if (l > 0.5f)
+        {
+            transform.localScale = new Vector3(l, l, l);
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+    }
+}
